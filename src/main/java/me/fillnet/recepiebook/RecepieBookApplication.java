9a7a86e7ab2763abd3cceb6cc6918cdc4b344b31
Fillnet <filipp@tenikaytis.ru>
2023-01-15 12:28:06 +0300

    First commit

---
+++ src/main/java/me/fillnet/recepiebook/RecepieBookApplication.java
@@ -0,0 +1,13 @@
+package me.fillnet.recepiebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepieBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecepieBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/fillnet/recepiebook/RecepieBookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fillnet.recepiebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepieBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
